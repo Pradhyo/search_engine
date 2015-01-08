f48da49929f72a162fb9541da0e6d95c8af419c6
Pradhyo Bijja <pradhyo@nyu.edu>
2015-01-08 12:24:42 +0530

    Add code to extract next link into a function

--- web_crawler.py
+++ web_crawler.py
@@ -1,9 +1,14 @@
 # Build a web crawler
 
+
+def get_next_target(page):
+	start_link = page.find('<a href=')
+	url_start = page.find('"',start_link)
+	url_end = page.find('"',url_start+1)
+	url= page[url_start+1:url_end]
+	return url, url_end
+
 page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">')
-start_link = page.find('<a href=')
-url_start = page.find('"',start_link)
-url_end = page.find('"',url_start+1)
-url= page[url_start+1:url_end]
+url, end_pos = get_next_target(page)
 print url
